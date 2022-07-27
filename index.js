2f22077e333f04bf58d4957863c5924b627aa0af
Serena Black <sunsetsin1.sy@gmail.com>
2022-07-27 19:46:18 +0545

    fixed issues and completed ex3.5-3.8

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express= require("express")
-
+const morgan=require("morgan")
 const App=express()
 const cors=require("cors")
+
+App.use(cors())
 App.use(express.json())
-const persons=[
+App.use(morgan((tokens,req,res)=>{
+  return[tokens.method(req,res),
+    tokens.url(req,res),
+    tokens.status(req,res),
+  tokens.res(req,res,"content-length"),"-",
+tokens['response-time'](req,res),'ms',
+JSON.stringify(req.body)].join(' ')
+}))
+
+let persons=[
     { 
       "id": 1,
       "name": "Arto Hellas", 
-      "number": "040-123456"
+      "number": 
+      "040-123456"
     },
     { 
       "id": 2,
@@ -25,13 +37,16 @@ const persons=[
       "number": "39-23-6423122"
     }
 ]
+
 App.get("/",(request,response)=>{
     response.send("<h1>Phonebook</h1>")
 })
+
 App.get("/persons",(request,response)=>{
     response.json(persons)
     
 })
+
 App.get("/info",(request,response)=>{
   console.log(response)
   const date=new Date
@@ -39,25 +54,32 @@ App.get("/info",(request,response)=>{
   response.send(`<h3>Phonebook has information of ${persons.length} people</h3>
   <h3>${date}</h3>`)
 })
+
 App.get("/persons/:id",(req,res)=>{
-  const id=Number(req.params.id)
-  
+  const id=Number(req.params.id) 
   const person=persons.find((x)=> 
   x.id===id)
   if (person) res.json(person)
   else res.status(404).send("No such id found.")
   })
-  App.post("/persons/",(req,res)=>{
+
+ App.post("/persons/",(req,res)=>{
     const personObj=req.body
+    if (personObj.name==="" || personObj.number==="" ){
+    res.status(404).json({error:"The name or number is missing."})
+    } else if (persons.filter((x)=>x.name===personObj.name).length!==0){
+    res.status(404).json({error:"The name must be unique."})}
+    else{
     personObj.id=Math.floor(Math.random()*1000000)
     persons.push(personObj)
     res.json(persons)
-  })
+  }})
 
   App.delete("/persons/:id",(req,res)=>{
     const id=Number(req.params.id)
     const delPerson=persons.filter((x)=>x.id!==id)
     res.json(delPerson)
   })
-  const port=3001
+
+const port=3001
 App.listen(port,()=>{console.log(`Server is running on part ${port}`)})
