6ac1b655ba7b6e886d7648ff5845e0837be40f5e
Serena Black <sunsetsin1.sy@gmail.com>
2022-08-01 18:36:20 +0545

    app connection with the database

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
 const express= require("express")
 const morgan=require("morgan")
 const App=express()
 const cors=require("cors")
+const Person = require('./models/person')
+
 
 App.use(express.static("build"))
 App.use(cors())
@@ -15,29 +18,29 @@ tokens['response-time'](req,res),'ms',
 JSON.stringify(req.body)].join(' ')
 }))
 
-let persons=[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": 
-      "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons=[
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": 
+//       "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 App.get("/",(request,response)=>{
     response.send("<h1>Phonebook</h1>")
