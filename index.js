0657cc0541eceb5bc6a9ece59a4b71f312683aa0
Serena Black <sunsetsin1.sy@gmail.com>
2022-08-03 15:17:16 +0545

    completed exercise 3.19

--- index.js
+++ index.js
@@ -122,11 +122,14 @@ App.put("/persons/:id", (req, res, next) => {
     name: personElem.name,
     number: personElem.number,
   };
-  Person.findByIdAndUpdate(req.params.id, person, { new: true }).then(
-    (updatedPerson) => {
-      res.json(updatedPerson);
-    }
-  );
+  Person.findByIdAndUpdate(req.params.id, person, {
+    new: true,
+    // ,
+    // runValidators: true,
+    // context: "query",
+  }).then((updatedPerson) => {
+    res.json(updatedPerson);
+  });
 });
 
 const errorHandler = (error, request, response, next) => {
--- models/person.js
+++ models/person.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log("connecting to", url);
 
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 //   const url = `mongodb+srv://phonebook-server:${password}@cluster0.3td7lci.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
-  const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number,
-    
-  })
-personSchema.set('toJSON', {
+const personSchema = new mongoose.Schema({
+  name: { type: String, minLength: 3, required: true },
+  number: Number,
+});
+personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema);
